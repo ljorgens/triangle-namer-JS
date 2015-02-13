d1776902194dd381cde806ae595fa412db83e0dd
ljorgens <leejorgensen22@gmail.com>
2015-02-13 10:42:28 -0800

    block any non-triangles

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 var triangleName = function(input_one, input_two, input_three){
 	var sides_array = [];
+	var input_one = parseInt(input_one);
+	var input_two = parseInt(input_two);
+	var input_three = parseInt(input_three);
+	if ((input_one + input_two <= input_three) || (input_three + input_two <= input_one) || (input_one + input_three <= input_two)) {
+			return ("not a triangle!");
+	} else {
+	}
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,12 @@
 describe('triangleName', function(){
 	it('will return whether it is a triangle or not', function(){
-		expect(triangleName([2, 2, 8])).to.equal("not a triangel!");	
+		expect(triangleName("2", "2", "8")).to.equal("not a triangle!");	
+	});
+	
+	it('will return whether it is a triangle or not', function(){
+		expect(triangleName("20", "2", "8")).to.equal("not a triangle!");	
+	});
+	it('will return whether it is a triangle or not', function(){
+		expect(triangleName("2", "20", "8")).to.equal("not a triangle!");	
 	});
 });
