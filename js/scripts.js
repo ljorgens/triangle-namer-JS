d4ea8a05987ef032fb866e0589b590fd939038e1
ljorgens <leejorgensen22@gmail.com>
2015-02-13 10:22:40 -0800

    initilize files

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var triangleName = function(input_one, input_two, input_three){
+	var sides_array = [];
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('triangleName', function(){
+	it('will return whether it is a triangle or not', function(){
+		expect(triangleName([2, 2, 8])).to.equal("not a triangel!");	
+	});
+});
