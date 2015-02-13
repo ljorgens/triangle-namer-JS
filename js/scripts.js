538b950f9c34100e6c711890cc61c573d8b8eef4
ljorgens <leejorgensen22@gmail.com>
2015-02-13 11:04:31 -0800

    add a website that will take input from a user

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,26 @@ var triangleName = function(input_one, input_two, input_three){
 	if ((input_one + input_two <= input_three) || (input_three + input_two <= input_one) || (input_one + input_three <= input_two)) {
 			return ("not a triangle!");
 	} else {
+		if (input_one === input_two && input_one === input_three){
+			return ("its an equilateral traingle!")
+		} else if (input_one === input_two || input_one === input_three || input_three === input_two) {
+			return ("its a isosceles traingle!")
+		} else {
+			return ("its a scalene triangle!")
+		}
 	}
-
 };
+
+$(document).ready(function(){
+	$("form#triangleName").submit(function(event){
+		var input_one = $("input#input_one").val();
+		var input_two = $("input#input_two").val();
+		var input_three = $("input#input_three").val();
+		var result = triangleName(input_one, input_two, input_three);
+		
+		$("#triangle_name").text(result);
+		
+		$(".result").show();
+		event.preventDefault();
+	});
+});
--- spec/specs.js
+++ spec/specs.js
@@ -6,7 +6,18 @@ describe('triangleName', function(){
 	it('will return whether it is a triangle or not', function(){
 		expect(triangleName("20", "2", "8")).to.equal("not a triangle!");	
 	});
+	
 	it('will return whether it is a triangle or not', function(){
 		expect(triangleName("2", "20", "8")).to.equal("not a triangle!");	
+	});
+	
+	it('will return equilateral if all sides are the same', function(){
+		expect(triangleName("2", "2", "2")).to.equal("its an equilateral traingle!")
+	});
+	it('will return isosceles if twos sides are the same', function(){
+		expect(triangleName("2", "3", "2")).to.equal("its a isosceles traingle!")
+	});
+	it('will return scalene if no sides are the same', function(){
+		expect(triangleName("3", "4", "5")).to.equal("its a scalene triangle!")
 	});
 });
